12e0bc90c4e36dc08e89842819d3f444eb6941f6
josh <jjboy2245@gmail.com>
2025-05-18 17:00:12 +0100

    Create texture.py

---
+++ avalanche_engine/components/texture.py
@@ -0,0 +1,21 @@
+from ..config import *
+from .component import Component
+
+class Texture(Component):
+    def __init__(self, texture_path):
+        super().__init__()
+
+        self.texture = pg.image.load(texture_path).convert_alpha()
+        self.texture = pg.transform.flip(self.texture, False, True)
+        self.image_data = pg.image.tobytes(self.texture, "RGBA", True)
+        self.width, self.height = self.texture.get_size()
+
+        self.engine_texture = None
+
+    def on_create(self):
+        self.engine_texture = self.engine.ctx.texture((self.width, self.height), 4, self.image_data)
+        self.engine_texture.build_mipmaps()
+
+    def on_update(self):
+        if self.engine_texture:
+            self.engine_texture.use(location=0)  # Needs to be bound every frame
